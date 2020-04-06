2dd68e301956d5aee9108f7be4beba484de46ba2
codecell <ezakaphil@gmail.com>
2020-04-06 13:41:27 +0100

    implement rubocop corrections

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@ source 'https://rubygems.org'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
-gem 'tty-font', '~> 0.5.0'
 gem 'colorize', '~> 0.8.1'
+gem 'tty-font', '~> 0.5.0'
--- bin/main.rb
+++ bin/main.rb
@@ -4,13 +4,13 @@ require 'tty-font'
 require 'colorize'
 
 def show_title
-  font = TTY::Font.new()
+  font = TTY::Font.new
   font_messge = font.write('Js Companion', letter_spacing: 1)
   puts font_messge
 end
 
-def Print_linter_result(error_bin, path)
-  puts "Working Tree Is Clean, No Errors Found in #{path}".green unless error_bin.length > 0
+def print_linter_result(error_bin, path)
+  puts "Working Tree Is Clean, No Errors Found in #{path}".green if !error_bin.empty?
   puts
 
   counter = 0
@@ -18,14 +18,14 @@ def Print_linter_result(error_bin, path)
 
   puts "#{number_of_errors} Type(s) of Error(s) Found!".yellow if number_of_errors.positive?
   while counter < number_of_errors
-    puts "#{counter + 1} #{error_bin[counter]} In The Path #{path}".red
+    puts "#{counter + 1}. #{error_bin[counter]} In The Path #{path}".red
     counter += 1
   end
 end
 
 def js_companion_init
   show_title
-  
+
   errors = []
 
   return unless ARGV.length.positive?
@@ -36,12 +36,10 @@ def js_companion_init
 
   if js_file_pattern.match?(path)
     LintWare.init_files_linting(path, errors)
-
-    Print_linter_result(errors, path)
   else
     LintWare.init_dir_linting(path, errors)
-    Print_linter_result(errors, path)
   end
+  print_linter_result(errors, path)
 end
 
 js_companion_init
